fa34aca00d87785db8b5d52ec233c9d39cc70724
Quan Nguyen <vozermuonnam1@gmail.com>
2023-10-28 08:42:11 +0700

    add users

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ const port = 3000
 app.get('/', (req, res) => res.json([{
     name: 'Quan',
     email: 'q@gmail.com'
-}]))
+},
+{
+  name: 'Tra',
+  email: 'tra@gmail.com'
+},
+{
+  name: 'Ngan',
+  email: 'Ngan@gmail.com'
+}
+]))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
